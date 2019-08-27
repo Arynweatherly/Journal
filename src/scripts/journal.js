1715cb018d6a0b3127414cf6229756839503fa66
aryn weatherly <arynweatherly@aryns-MacBook-Pro.local>
2019-08-27 17:38:40 -0500

    Journal2

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,8 +1,24 @@
-let journalEntry =
-{
-    journalDate: [],
-    conceptsCovered: [],
-    content: [],
-    emotions: [],
+const journalEntry = []
+
+journalEntry.push {
+    dateOfEntry: "August, 13,2019",
+    conceptsCovered: "command line, linking css and HTML",
+    journalEntry: "Today we started out learning about command line interface commands. We then did the practice exercises which i felt really good about. I created a help command and also learned how to use the tree command. We then started working with CSS style sheets and linked it to our HTML sheet to change the color of our website we were working on. The final practice exercise was a bit of a challenge for me at first but i figured it out eventually.",
+    moodOfTheDay:"Happy",
+}
+journalEntry.push {
+    dateOfEntry: "August 14, 2019",
+    conceptsCovered: "flexbox, github flow",
+    journalEntry: "today we did the Wizard of Oz assignment. we also learned about flexbox which seemed slightly confusing at first. We also went over the chapter 8 material. This was the first day we discussed the github flow which was very confusing at first. There were come major commitmment issues and overall people were struggling.",
+    moodOfTheDay: "excited",
 }
 
+journalEntry.push {
+    dateOfEntry: "August 26, 2019",
+    conceptsCovered: "javascript objects and arrays",
+    journalEntry: "today we focused on beginning javascript topics such as objects and arrays. We worked on the second part of making our journal which seemed a bit confusing at first but after asking mo about it, everything was cleared up. I felt pretty perplexed today in all honesty. I got to school late so i felt behind, but i stayed later and worked on some of the assignments. I also took lots of notes toward the end of the day which did help a bit.",
+    moodOfTheDay: "perplexed",
+
+}
+
+console.log("journalEntry", journalEntry);
